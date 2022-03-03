452e67d7e5fec253a804f7c27c912fd5d62f603e
潘涔洋 <pancenyang@apexsoft.com.cn>
2022-03-03 18:06:44 +0800

    提交代码，有飘红，目前不知道如何解决

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,18 @@
  * @Date: 2022-03-03 11:30:32
  * @LastEditTime: 2022-03-03 14:52:26
 -->
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-</script>
 
 <template>
   <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script lang="ts">
+import { ref, provide } from "vue";
+export default {
+  name: "App",
+  setup(props) {
+    const menuVisible = ref(true);
+    provide("menuVisible", menuVisible);
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,18 @@
  * @Date: 2022-03-03 14:55:02
  * @LastEditTime: 2022-03-03 14:56:46
 -->
+<script setup>
+import TopnavVue from "../components/Topnav.vue";
+</script>
 <template>
   <div>
     <div class="topnavAndBanner">
-      <!-- <Topnav /> -->
+      <TopnavVue />
       <div class="banner">
         <h1>轱辘UI</h1>
         <h2>一个厉害的 UI 框架</h2>
         <p class="actions">
-          <a href="https://github.com">GitHub</a>
+          <a href="https://github.com/pcy999/vue3Gulu">GitHub</a>
           <router-link to="/doc">开始</router-link>
         </p>
       </div>
